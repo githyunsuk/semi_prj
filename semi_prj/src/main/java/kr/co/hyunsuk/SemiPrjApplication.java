8c182d67fc1160d6b555f311d5821a2ab652d877
Hyunsuk Ju <wngustjr1306@naver.com>
2025-08-05 22:27:51 +0900

    first commit

---
+++ semi_prj/src/main/java/kr/co/hyunsuk/SemiPrjApplication.java
@@ -0,0 +1,13 @@
+package kr.co.hyunsuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemiPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemiPrjApplication.class, args);
+	}
+
+}
---
+++ semi_prj/src/test/java/kr/co/hyunsuk/SemiPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.hyunsuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemiPrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
